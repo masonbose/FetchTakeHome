58a0320cde69da9cabc4c91200e70af27473d0fb
Mason Bose <masonbose@admins-MacBook-Pro.local>
2023-09-13 14:17:44 -0500

    initial commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Mason Bose  on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
